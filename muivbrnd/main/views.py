ca8798dc557438f4ab86ed09c6cd53a37a21faac
Zect 愛 <ilyaking.just@yandex.ru>
2024-03-01 13:34:19 +0300

    smth changes before add db tables

--- muivbrnd/main/views.py
+++ muivbrnd/main/views.py
@@ -5,10 +5,13 @@ def index(request):
     data = {
         'title': 'Главная страница',
         'values': ['This will be', 'information section', 'but now it is just text'],
-        'goods': ['Prod1', 'Prod2', 'Prod3',
-                  'Prod4', 'Prod5', 'Prod6',
-                  'Prod7', 'Prod8',
-                 ]
+        'goods': ['Product_1', 'Product_2', 'Product_3',
+                  'Product_4', 'Product_5', 'Product_6',
+                  'Product_7', 'Product_8',
+                 ],
+        'instruction': ['Действие_1', 'Действие_2', 'Действи_3',
+                        'Действие_4', 
+                ],
     }
     return render(request, 'main/index.html', data)
 
